ec56ce425a0fc79e93c0e3e481198615dd467af9
Oluwaseun Sodimu <seun.sodimu@yahoo.com>
2024-01-12 12:19:54 -0600

    Update cert for azure database

--- db.php
+++ db.php
@@ -14,11 +14,14 @@ $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::MYSQL_ATTR_SSL_CA       => 'certs/DigiCertGlobalRootCA.crt.pem', // Path to your CA certificate
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 ];
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    error_log($e->getMessage()); 
+    die('Connection failed: Unable to connect to the database');
 }
 ?>
